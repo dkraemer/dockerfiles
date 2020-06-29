55fb7e32f36395028b7c0c419f448f60527d2fa8
Daniel Kraemer <dkraemer@dkross.org>
2020-06-29 04:58:27 +0200

    Map port for firebase auth callback

--- dev/web/run.ps1
+++ dev/web/run.ps1
@@ -1,1 +1,1 @@
-docker run -it --rm --name dev-web -p 4200:4200/tcp -p 8080:8080/tcp dkraemer/dev-web:latest
+docker run -it --rm --name dev-web -p 4200:4200/tcp -p 8080:8080/tcp -p 9005:9005/tcp dkraemer/dev-web:latest
