deb99fc6f26e0621f973ee45f1fd55bbb6290d64
Daniel Kraemer <dkraemer@dkross.org>
2020-07-01 16:17:55 +0200

    Remove angular image

--- dev/angular/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build --rm --pull -t dkraemer/dev-angular:latest .
--- dev/angular/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm --name dev-angular -p 4200:4200/tcp -p 8080:8080/tcp -p 9005:9005/tcp dkraemer/dev-angular:latest
